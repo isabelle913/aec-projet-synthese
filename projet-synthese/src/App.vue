733447e93e2d6afd37d2afe17b07aa72ccc66356
isabelle913 <atelierisabelle@videotron.ca>
2024-03-07 20:49:28 -0500

    Débuter layout principale

--- projet-synthese/src/App.vue
+++ projet-synthese/src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <p class="flex justify-center items-center text-3xl h-screen">Bonjour super team</p>
+  <div class="container mx-auto">
+    <!-- Header -->
+    <!-- menu -->
+    <!-- <RouterView /> -->
+    <p class="flex justify-center items-center text-3xl h-screen">Bonjour super team</p>
+    <!-- Footer -->
+  </div>
 </template>
 
 <script setup></script>
