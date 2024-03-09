3a6f6c4459f01f6832c20568ab517d4cf590b116
isabelle913 <atelierisabelle@videotron.ca>
2024-03-09 11:21:27 -0500

    Création card entreprise

---
+++ projet-synthese/src/components/CardEntreprise.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex-none bg-white p-4 w-[500px] h-[250px] flex cursor-pointer" @click="onDetails(entreprise._id)">
+    <div class="w-1/2 flex grid place-content-center p-6">
+      <img :src="entreprise.image" :alt="`Logo de l'entreprise ${entreprise.name}`" />
+    </div>
+    <div class="w-1/2 flex flex-col justify-center p-4">
+      <div class="font-bold text-xl">{{ entreprise.name }}</div>
+      <div class="text-gray-400 text-sm">{{ entreprise.address }}</div>
+      <div class="text-gray-400 text-sm">{{ entreprise.city }}, {{ entreprise.postalCode }}</div>
+      <div class="text-gray-400 text-sm mt-4">Téléphone: {{ entreprise.phone }}</div>
+      <div class="text-gray-400 text-sm">Télécopieur: {{ entreprise.phone }}</div>
+    </div>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  entreprise: {
+    type: Object,
+    required: true,
+  },
+});
+
+function onDetails(idString) {
+  const id = Number(idString);
+  console.log("onDetails", id, typeof id);
+  // TODO utiliser routing
+}
+</script>
+<style scoped>
+img {
+  width: 100%;
+}
+</style>
