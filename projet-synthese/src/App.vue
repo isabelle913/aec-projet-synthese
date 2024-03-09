27b947c8098a6256d62fdc74497a47f8af87221d
isabelle913 <atelierisabelle@videotron.ca>
2024-03-09 11:20:26 -0500

    temporairement pour voir ma page

--- projet-synthese/src/App.vue
+++ projet-synthese/src/App.vue
@@ -1,7 +1,10 @@
 <template>
-  <p class="flex justify-center items-center text-3xl h-screen">Bonjour super team</p>
+  <!-- <p class="flex justify-center items-center text-3xl h-screen">Bonjour super team</p> -->
+  <Entreprise></Entreprise>
 </template>
 
-<script setup></script>
+<script setup>
+import Entreprise from "./views/Entreprises.vue";
+</script>
 
 <style scoped></style>
