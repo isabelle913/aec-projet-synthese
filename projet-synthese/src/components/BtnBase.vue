7a3c4a956bed6c19c423bf0fe2ff0f9ed499f312
isabelle913 <atelierisabelle@videotron.ca>
2024-03-09 11:20:47 -0500

    Création du bouton

---
+++ projet-synthese/src/components/BtnBase.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="my-6 p-4 rounded-lg" :style="{ backgroundColor: color }" @click="action">{{ title }}</button>
+</template>
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    default: "#0c9da3", // TODO remplacer par la variable
+  },
+  action: {
+    type: Function,
+  },
+});
+</script>
+<style></style>
