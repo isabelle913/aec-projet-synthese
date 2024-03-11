8eb50901ad2d9761c170617e070a6613790bb30c
Moussa Abdi Diraneh <moise2015@hotmail.fr>
2024-03-10 20:46:33 -0400

    Ajout du Menu Action, du Frame pour les autres pages

--- projet-synthese/src/App.vue
+++ projet-synthese/src/App.vue
@@ -1,7 +1,26 @@
 <template>
-  <p class="flex justify-center items-center text-3xl h-screen">Bonjour super team</p>
+  <div class="grid grid-cols-12 gap-5 my-8 container">
+    <div class="col-span-12 md:col-span-2 max-md:p-10 my-4 bg-slate-400">
+      <Navigation />
+    </div>
+    <div class="col-span-12 md:col-span-10 max-md:p-10 my-4 bg-red-300">
+      <div
+        class="col-span-12 md:col-span-8 max-md:p-10 p-16 my-10 bg-slate-300 menuAction"
+      >
+        <MenuAction />
+      </div>
+      <div
+        class="col-span-12 md:col-span-8 max-md:p-10 p-16 bg-slate-300 variableSection"
+      >
+        <router-view :identification="identification" />
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import Navigation from "./components/Navigation.vue";
+import MenuAction from "./components/MenuAction.vue";
+</script>
 
 <style scoped></style>
