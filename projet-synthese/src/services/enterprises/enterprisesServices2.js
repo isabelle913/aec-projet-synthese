6a67e2e7978333b080b6097e0cc1ec1ae45948a7
isabelle913 <atelierisabelle@videotron.ca>
2024-03-19 04:52:43 -0400

    Tentative de get

--- projet-synthese/src/services/enterprises/enterprisesServices2.js
+++ projet-synthese/src/services/enterprises/enterprisesServices2.js
@@ -1,22 +1,24 @@
-import { ref } from "vue";
+// import { ref } from "vue";
 
-export function useEnterpriseServices(){
+export function useEnterpriseServices() {
+  function getEnterprises() {
+    // const liste = ref([]);
 
-    function getEnterprises(){
-        const liste = ref([]);
+    fetch("https://aec-projet-integrateur-api.fly.dev/enterprises/")
+      .then((response) => response.json())
+      .then((data) => {
+        // liste.value = data;
+        // console.log("Liste des entreprises:", liste.value);
 
-        fetch("https://aec-projet-integrateur-api.fly.dev/enterprises/")
-            .then(response => response.json())
-            .then(data => {
-                liste.value = data;
-                console.log('Liste des entreprises:', liste.value);
-            })
-            .catch(error => {
-                console.error("Une erreur s'est produite lors de la récupération des données:", error);
-            });
+        if (!data) throw new Error("Pas de data");
 
-        return liste.value;
-    }
+        console.log("data", data);
+        return data;
+      })
+      .catch((error) => {
+        console.error("Une erreur s'est produite lors de la récupération des données:", error);
+      });
+  }
 
-    return { getEnterprises }
+  return { getEnterprises };
 }
