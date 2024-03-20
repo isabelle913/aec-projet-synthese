98f9b8405d3aa20a1e48f07f956e4c28086ca3aa
isabelle913 <atelierisabelle@videotron.ca>
2024-03-20 05:59:34 -0400

    started only

---
+++ projet-synthese/src/components/ApercuRapide.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex">
+    <div class="bg-yellow-200">Demandes de stage</div>
+    <div class="bg-red-200">Offres de stage</div>
+    <div class="bg-purple-200">Candidats</div>
+  </div>
+</template>
+<script setup></script>
