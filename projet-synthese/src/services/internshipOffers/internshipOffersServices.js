09e5d6c1c6385b6962c57d65a95ef9d95b3b1317
isabelle913 <atelierisabelle@videotron.ca>
2024-03-23 08:07:39 -0400

    modified name of liste

--- projet-synthese/src/services/internshipOffers/internshipOffersServices.js
+++ projet-synthese/src/services/internshipOffers/internshipOffersServices.js
@@ -1,32 +1,32 @@
 import { ref } from "vue";
 
 export default function InternshipOffersService() {
-  const liste = ref([]);
+  const listeOffers = ref([]);
   const objet = ref({});
   const success = ref(false);
 
   const allInternshipOffers = () => {
     fetch("https://aec-projet-integrateur-api.fly.dev/internship-offers/")
-      .then(response => response.json())
-      .then(data => {
-        liste.value = data;
-        console.log('Liste des offres de stages:', liste.value);
+      .then((response) => response.json())
+      .then((data) => {
+        listeOffers.value = data;
+        console.log("Liste des offres de stages:", listeOffers.value);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Une erreur s'est produite lors de la récupération des données:", error);
       });
-      return liste.value;
+    return listeOffers.value;
   };
 
   const getInternshipOffereById = (_id) => {
     return fetch(`https://aec-projet-integrateur-api.fly.dev/internship-offers/${_id}`)
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         objet.value = data;
-        console.log('Entreprise trouvée :', objet.value);
+        console.log("Entreprise trouvée :", objet.value);
         return objet.value;
       })
-      .catch(error => {
+      .catch((error) => {
         console.log("Une erreur s'est produite lors de la récupération des données:", error);
         throw error;
       });
@@ -35,29 +35,29 @@ export default function InternshipOffersService() {
   const addInternshipOffer = async (newInternshipOffer) => {
     try {
       const response = await fetch("https://aec-projet-integrateur-api.fly.dev/enterprises/", {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(newInternshipOffer),
       });
 
       if (response.ok) {
         success.value = true;
-        console.log('Requête POST réussie !');
+        console.log("Requête POST réussie !");
         // Vous pouvez ajouter ici d'autres actions après le succès de la requête.
       } else {
-        console.error('Échec de la requête POST.');
+        console.error("Échec de la requête POST.");
         success.value = false;
       }
     } catch (error) {
-      console.error('Erreur lors de la requête POST:', error);
+      console.error("Erreur lors de la requête POST:", error);
       success.value = false;
     }
   };
 
   return {
-    liste,
+    listeOffers,
     objet,
     success,
     allInternshipOffers,
