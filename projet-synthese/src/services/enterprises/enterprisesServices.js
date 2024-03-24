0f8ba4d762000c7db31f892f23ffffff1bad166c
isabelle913 <atelierisabelle@videotron.ca>
2024-03-24 10:45:43 -0400

    changed little things

--- projet-synthese/src/services/enterprises/enterprisesServices.js
+++ projet-synthese/src/services/enterprises/enterprisesServices.js
@@ -1,19 +1,22 @@
 import { ref } from "vue";
+import { useRouter } from "vue-router";
 
 export default function EnterpriseService() {
   const enterpriseListe = ref([]);
   const objet = ref({});
   const success = ref(false);
 
+  const router = useRouter();
+
   const allEnterprises = () => {
     return fetch("https://aec-projet-integrateur-api.fly.dev/enterprises/")
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         enterpriseListe.value = data;
-        console.log('Liste des entreprises:', enterpriseListe.value);
+        console.log("Liste des entreprises:", enterpriseListe.value);
         return enterpriseListe.value;
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Une erreur s'est produite lors de la récupération des données:", error);
         throw error;
       });
@@ -21,80 +24,80 @@ export default function EnterpriseService() {
 
   const getEntrepriseById = (_id) => {
     return fetch(`https://aec-projet-integrateur-api.fly.dev/enterprises/${_id}`)
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         objet.value = data;
-        console.log('Entreprise trouvée :', objet.value);
+        console.log("Entreprise trouvée :", objet.value);
         return objet.value;
       })
-      .catch(error => {
+      .catch((error) => {
         console.log("Une erreur s'est produite lors de la récupération des données:", error);
         throw error;
       });
   };
 
-  const addEnterprises = async () => {
+  const addEnterprises = async (newEnterprise) => {
     try {
       const response = await fetch("https://aec-projet-integrateur-api.fly.dev/enterprises/", {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(),
+        body: JSON.stringify(newEnterprise),
       });
-
-      console.log(body);
 
       if (response.ok) {
         success.value = true;
-        console.log('Requête POST réussie !');
+        console.log("Requête POST réussie !");
       } else {
-        console.error('Échec de la requête POST.');
+        console.error("Échec de la requête POST.");
         success.value = false;
       }
     } catch (error) {
-      console.error('Erreur lors de la requête POST:', error);
+      console.error("Erreur lors de la requête POST:", error);
       success.value = false;
     }
   };
 
-  const editEnterprises = async (_id) =>{
+  const editEnterprises = async (data) => {
     try {
-      const response = await fetch(`https://aec-projet-integrateur-api.fly.dev/enterprises/${_id}`,{
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(),
-      });
-
-      if(response.ok){
-        success.value = true;
-        console.log('Requête PATCH réussie !')
-      } else{
-        console.error('Échec de la requête PATCH:', error);
-        success.value = false;
-      }
-    }catch(error){
-
-    }
-  };
-
-  const deleteEnterprise = async (_id) => {
-    try {
-      const response = await fetch(`https://aec-projet-integrateur-api.fly.dev/enterprises/${_id}`, {
-        method: 'DELETE',
+      const response = await fetch(`https://aec-projet-integrateur-api.fly.dev/enterprises/${data._id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
       });
 
       if (response.ok) {
         success.value = true;
-        console.log('Requête DELETE réussie !');
+        console.log("Requête PATCH réussie !");
       } else {
-        console.error('Échec de la requête DELETE.');
+        // console.error("Échec de la requête PATCH:", error);
         success.value = false;
       }
     } catch (error) {
-      console.error('Erreur lors de la requête DELETE:', error);
+      console.log(error);
+    }
+  };
+
+  const deleteEnterprise = async (_id) => {
+    console.log("deleteEnterprise", _id);
+    try {
+      const response = await fetch(`https://aec-projet-integrateur-api.fly.dev/enterprises/${_id}`, {
+        method: "DELETE",
+      });
+
+      if (response.ok) {
+        success.value = true;
+        console.log("Requête DELETE réussie !");
+        router.push({ name: "enterprises" });
+      } else {
+        console.error("Échec de la requête DELETE.");
+        success.value = false;
+      }
+    } catch (error) {
+      console.error("Erreur lors de la requête DELETE:", error);
       success.value = false;
     }
   };
