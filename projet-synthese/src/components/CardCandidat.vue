bf2f51e69bf6b6191fbbf061de2e677074c4b81e
delfynn2kx <hubert.delphine@gmail.com>
2024-03-24 21:34:09 -0400

    modification de cardCandidat il y a un bogue entre flex et grid dans cette page

--- projet-synthese/src/components/CardCandidat.vue
+++ projet-synthese/src/components/CardCandidat.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="flex-none bg-white p-4 w-[500px] h-[250px] flex cursor-pointer" @click="onDetails(candidat._id)">
-    <div class="w-1/2 flex grid place-content-center p-6">
+    <div class="w-1/2 place-content-center p-6">
       <img :src="candidat.image" :alt="`Photo du candidat ${candadit.name}`" />
     </div>
     <div class="w-1/2 flex flex-col justify-center p-4">
       <div class="font-bold text-xl">{{ candidat.name }}</div>
-      <div class="text-gray-400 text-sm">{{ candidat.address }}</div>
-      <div class="text-gray-400 text-sm">{{ candidat.city }}, {{ candidat.postalCode }}</div>
+      <div class="text-gray-400 text-sm">{{ candidat.titre }}</div>
+      <div class="text-gray-400 text-sm">{{ candidat.courriel }}</div>
       <div class="text-gray-400 text-sm mt-4">Téléphone: {{ candidat.phone }}</div>
-      <div class="text-gray-400 text-sm">Télécopieur: {{ candidat.phone }}</div>
     </div>
   </div>
 </template>
