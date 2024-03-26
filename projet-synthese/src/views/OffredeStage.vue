5860782500e2807f05695aa46b83a71bbc7bfaaa
imane sbiti <emysb2020@gmail.com>
2024-03-26 19:27:02 -0400

    offfre de stage

---
+++ projet-synthese/src/views/OffredeStage.vue
@@ -0,0 +1,134 @@
+<template>
+
+    <RouterLink  class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 border border-orange-700 rounded" to="AjoutOffredeStage">Ajouter une offre de stage</RouterLink>
+    
+    <div class=" shadow-md sm:rounded-lg mt-16">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Poste
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Secteur d'activité
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Ville
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Date d'inscription
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <p class="text-red-600 font-bold">Intégrateur Web<span class="text-xs/3"> (inactivée)</span></p>
+                    <p>Acolyte Communication</p>
+                </th>
+                <td class="px-6 py-4">
+                    Nouvelles technologies
+                </td>
+                <td class="px-6 py-4">
+                    Trois-Rivières
+                </td>
+                <td class="px-6 py-4">
+                    2024-02-30
+                </td>
+                <td class="px-6 py-4">
+                    <button><span class="pr-2 mdi mdi-eye-circle text-blue-600 text-lg mr-4"></span></button>
+                    <button><span class="pr-2 mdi mdi-file-edit text-yellow-600 text-lg mr-4"></span></button>
+                    <button><span class="pr-2 mdi mdi-delete-circle text-red-600 text-lg mr-4"></span></button>
+                </td>
+            </tr>
+            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <p class="font-bold">Intégrateur Web</p>
+                    <p>Acolyte Communication</p>
+                </th>
+                <td class="px-6 py-4">
+                    Nouvelles technologies
+                </td>
+                <td class="px-6 py-4">
+                    Trois-Rivières
+                </td>
+                <td class="px-6 py-4">
+                    2024-02-30
+                </td>
+                <td class="px-6 py-4">
+                    <button><span class="pr-2 mdi mdi-eye-circle text-blue-600 text-lg mr-4"></span></button>
+                    <button><span class="pr-2 mdi mdi-file-edit text-yellow-600 text-lg mr-4"></span></button>
+                    <button><span class="pr-2 mdi mdi-delete-circle text-red-600 text-lg mr-4"></span></button>
+                </td>
+            </tr>
+            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <p class="text-red-600 font-bold">Intégrateur Web<span class="text-xs/3"> (inactivée)</span></p>
+                    <p>Acolyte Communication</p>
+                </th>
+                <td class="px-6 py-4">
+                    Nouvelles technologies
+                </td>
+                <td class="px-6 py-4">
+                    Trois-Rivières
+                </td>
+                <td class="px-6 py-4">
+                    2024-02-30
+                </td>
+                <td class="px-6 py-4">
+                    <button><span class="pr-2 mdi mdi-eye-circle text-blue-600 text-lg mr-4"></span></button>
+                    <button><span class="pr-2 mdi mdi-file-edit text-yellow-600 text-lg mr-4"></span></button>
+                    <button><span class="pr-2 mdi mdi-delete-circle text-red-600 text-lg mr-4"></span></button>
+                </td>
+            </tr>
+            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <p class="font-bold">Intégrateur Web</p>
+                    <p>Acolyte Communication</p>
+                </th>
+                <td class="px-6 py-4">
+                    Nouvelles technologies
+                </td>
+                <td class="px-6 py-4">
+                    Trois-Rivières
+                </td>
+                <td class="px-6 py-4">
+                    2024-02-30
+                </td>
+                <td class="px-6 py-4">
+                    <button><span class="pr-2 mdi mdi-eye-circle text-blue-600 text-lg mr-4"></span></button>
+                    <button><span class="pr-2 mdi mdi-file-edit text-yellow-600 text-lg mr-4"></span></button>
+                    <button><span class="pr-2 mdi mdi-delete-circle text-red-600 text-lg mr-4"></span></button>
+                </td>
+            </tr>
+            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <p class="font-bold">Intégrateur Web</p>
+                    <p>Acolyte Communication</p>
+                </th>
+                <td class="px-6 py-4">
+                    Nouvelles technologies
+                </td>
+                <td class="px-6 py-4">
+                    Trois-Rivières
+                </td>
+                <td class="px-6 py-4">
+                    2024-02-30
+                </td>
+                <td class="px-6 py-4">
+                    <button><span class="pr-2 mdi mdi-eye-circle text-blue-600 text-lg mr-4"></span></button>
+                    <button><span class="pr-2 mdi mdi-file-edit text-yellow-600 text-lg mr-4"></span></button>
+                    <button><span class="pr-2 mdi mdi-delete-circle text-red-600 text-lg mr-4"></span></button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+</template>
+<script>
+
+</script>
+<style></style>
---
+++ projet-synthese/src/views/OffredeStageValidation.vue
@@ -0,0 +1,146 @@
+<template>
+    <h3 class="font-bold">Dernière <span class="text-red-600">Offres</span> de stage</h3>
+    <p class="text-red-600 text-base mb-20">En attente de validation</p>
+    
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Poste
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Ville
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Date
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Actions
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <p>Intégrateur Web</p>
+                    <p>Acolyte Communication</p>
+                </th>
+                <td class="px-6 py-4">
+                    Trois-Rivières
+                </td>
+                <td class="px-6 py-4">
+                    2024-02-30
+                </td>
+                <td class="px-6 py-4">
+                    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded mr-2">
+                        Refuser
+                    </button>
+                    <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-green-700 rounded mr-6">
+                        Accepter
+                    </button>
+                    <button><span class="pr-2 mdi mdi-eye-circle text-blue-600 text-lg mr-4"></span></button>
+                    <button><span class="pr-2 mdi mdi-file-edit text-yellow-600 text-lg mr-4"></span></button>
+                    <button><span class="pr-2 mdi mdi-delete-circle text-red-600 text-lg mr-4"></span></button>
+                </td>
+            </tr>
+            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <p>Intégrateur Web</p>
+                    <p>Acolyte Communication</p>
+                </th>
+                <td class="px-6 py-4">
+                    Trois-Rivières
+                </td>
+                <td class="px-6 py-4">
+                    2024-02-30
+                </td>
+                <td class="px-6 py-4">
+                    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded mr-2">
+                        Refuser
+                    </button>
+                    <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-green-700 rounded  mr-6">
+                        Accepter
+                    </button>
+                    <button><span class="pr-2 mdi mdi-eye-circle text-blue-600 text-lg mr-4"></span></button>
+                    <button><span class="pr-2 mdi mdi-file-edit text-yellow-600 text-lg mr-4"></span></button>
+                    <button><span class="pr-2 mdi mdi-delete-circle text-red-600 text-lg mr-4"></span></button>
+                </td>
+            </tr>
+            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <p>Intégrateur Web</p>
+                    <p>Acolyte Communication</p>
+                </th>
+                <td class="px-6 py-4">
+                    Trois-Rivières
+                </td>
+                <td class="px-6 py-4">
+                    2024-02-30
+                </td>
+                <td class="px-6 py-4">
+                    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded mr-2">
+                        Refuser
+                    </button>
+                    <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-green-700 rounded mr-6">
+                        Accepter
+                    </button>
+                    <button><span class="pr-2 mdi mdi-eye-circle text-blue-600 text-lg mr-4"></span></button>
+                    <button><span class="pr-2 mdi mdi-file-edit text-yellow-600 text-lg mr-4"></span></button>
+                    <button><span class="pr-2 mdi mdi-delete-circle text-red-600 text-lg mr-4"></span></button>
+                </td>
+            </tr>
+            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <p>Intégrateur Web</p>
+                    <p>Acolyte Communication</p>
+                </th>
+                <td class="px-6 py-4">
+                    Trois-Rivières
+                </td>
+                <td class="px-6 py-4">
+                    2024-02-30
+                </td>
+                <td class="px-6 py-4">
+                    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded mr-2">
+                        Refuser
+                    </button>
+                    <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-green-700 rounded mr-6">
+                        Accepter
+                    </button>
+                    <button><span class="pr-2 mdi mdi-eye-circle text-blue-600 text-lg mr-4"></span></button>
+                    <button><span class="pr-2 mdi mdi-file-edit text-yellow-600 text-lg mr-4"></span></button>
+                    <button><span class="pr-2 mdi mdi-delete-circle text-red-600 text-lg mr-4"></span></button>
+                </td>
+            </tr>
+            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <p>Intégrateur Web</p>
+                    <p>Acolyte Communication</p>
+                </th>
+                <td class="px-6 py-4">
+                    Trois-Rivières
+                </td>
+                <td class="px-6 py-4">
+                    2024-02-30
+                </td>
+                <td class="px-6 py-4">
+                    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded mr-2">
+                        Refuser
+                    </button>
+                    <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-green-700 rounded mr-6">
+                        Accepter
+                    </button>
+                    <button><span class="pr-2 mdi mdi-eye-circle text-blue-600 text-lg mr-4"></span></button>
+                    <button><span class="pr-2 mdi mdi-file-edit text-yellow-600 text-lg mr-4"></span></button>
+                    <button><span class="pr-2 mdi mdi-delete-circle text-red-600 text-lg mr-4"></span></button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+</template>
+<script>
+
+</script>
+<style></style>
