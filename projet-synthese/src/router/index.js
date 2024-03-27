c5d23c9e5294bb32cbeb2636262b63264770214f
isabelle913 <atelierisabelle@videotron.ca>
2024-03-26 21:15:59 -0400

    modifier fichier rooting

--- projet-synthese/src/router/index.js
+++ projet-synthese/src/router/index.js
@@ -1,12 +1,35 @@
 import { createRouter, createWebHistory } from "vue-router";
-// import HomeView from "../views/HomeView.vue";
+import Authentificator from "../components/Authentificator.vue";
 import Entreprises from "@/views/Entreprises.vue";
 import EntreprisesDetails from "@/views/EntreprisesDetails.vue";
 import TableauBord from "@/views/TableauBord.vue";
+import DemandeStagePage1 from "../views/DemandeStagePage1.vue";
+import DemandeStagePage2 from "../views/DemandeStagePage2.vue";
+import AjoutDemandeStage from "../views/AjoutDemandeStage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: "/",
+      name: "Authentificator",
+      component: Authentificator,
+    },
+    {
+      path: "/DemandeStagePage1",
+      name: "demandestagepage1",
+      component: DemandeStagePage1,
+    },
+    {
+      path: "/DemandeStagePage2",
+      name: "demandestagepage2",
+      component: DemandeStagePage2,
+    },
+    {
+      path: "/AjoutDemandeStage",
+      name: "ajoutdemandestage",
+      component: AjoutDemandeStage,
+    },
     {
       path: "/tableau-bord",
       name: "tableau-bord",
