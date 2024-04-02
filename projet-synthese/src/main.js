cdc651216681b757b9ef96c3cd8969d06df1dca2
isabelle913 <atelierisabelle@videotron.ca>
2024-04-02 05:24:14 -0400

    retirer pinia de main.js

--- projet-synthese/src/main.js
+++ projet-synthese/src/main.js
@@ -1,14 +1,11 @@
 import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
 const app = createApp(App);
 
-// Utiliser Pinia dans l'application
-app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
