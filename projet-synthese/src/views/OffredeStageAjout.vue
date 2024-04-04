b7261b4b2749a128dfd161294729bbe760da49c2
imane sbiti <emysb2020@gmail.com>
2024-04-04 12:54:49 -0400

    page modification fonctionnelle et validation d ajout fonctionnelle

--- projet-synthese/src/views/OffredeStageAjout.vue
+++ projet-synthese/src/views/OffredeStageAjout.vue
@@ -2,7 +2,7 @@
     <div class="DemandeStage__Titre">
       <h1>Ajouter une offre de stage</h1>
     </div>
-  <form>
+
     <section class="bg-white p-10 DemandeStage__section">
       <div v-if="showSuccessMessage" class="text-green-500 mt-4">
         Offre de stage ajoutée avec succès !
@@ -16,8 +16,9 @@
             type="text"
             v-model="title"
             name="title"
-            required
+            id="title"
           />
+          <p v-if="isError.title" class="col-start-4 col-span-9 text-red-500 text-md italic">Veuillez inscrire le titre de l'offre</p>
         </div>
 
         <div class="mb-16 mr-2">
@@ -26,18 +27,18 @@
             class="shadow appearance-none border rounded w-full py-7 px-3 mt-5 leading-tight focus:outline-none focus:shadow-outline input"
             placeholder="Ex.: Besoin d'un intégrateur Front-end Junior"
             type="text"
-            v-model="description" name="description"
-            required
+            v-model="description" name="description" id="description"
           />
+          <p v-if="isError.description" class="col-start-4 col-span-9 text-red-500 text-md italic">Veuillez inscrire la description de l'offre</p>
         </div>
 
-  
         <div class="mb-16 mr-2">
           <label class="text-bg font-bold" for="enterprise">Nom de l'enterprise</label>
-          <select v-model="selectedEnterprise"  name="selectedEnterprise" class="shadow appearance-none border rounded w-full py-7 px-3 mt-5 leading-tight focus:outline-none focus:shadow-outline input">
+          <select v-model="selectedEnterprise" name="selectedEnterprise" class="shadow appearance-none border rounded w-full py-7 px-3 mt-5 leading-tight focus:outline-none focus:shadow-outline input">
             <option disabled value="">Sélectionnez une enterprise</option>
             <option v-for="enterprise in enterpriseListe" :key="enterprise._id" :value="enterprise._id">{{ enterprise.name }}</option>
           </select>
+          <p v-if="isError.selectedEnterprise" class="col-start-4 col-span-9 text-red-500 text-md italic">Veuillez séléctionner une entreprise</p>
         </div>
   
         <h3>Informations sur l'enterprise</h3>
@@ -51,7 +52,7 @@
                   class="shadow appearance-none border rounded w-full py-7 px-3 mt-5 leading-tight focus:outline-none focus:shadow-outline input"
                   type="text"
                   v-model="description_enterprise"
-                  required
+                  readonly 
                 />
               
      
@@ -64,7 +65,7 @@
                   class="shadow appearance-none border rounded w-full py-7 px-3 mt-5 leading-tight focus:outline-none focus:shadow-outline input"
                   type="text"
                   v-model="city_enterprise"
-                  required
+                  readonly
                 />
               </div>
             </div>
@@ -77,7 +78,7 @@
                   class="shadow appearance-none border rounded w-full py-7 px-3 mt-5 leading-tight focus:outline-none focus:shadow-outline input"
                   type="text"
                   v-model="phone_enterprise"
-                  required
+                  readonly
                 />
               </div>
             </div>
@@ -88,7 +89,7 @@
                   class="shadow appearance-none border rounded w-full py-7 px-3 mt-5 leading-tight focus:outline-none focus:shadow-outline input"
                   type="text"
                   v-model="email_enterprise"
-                  required
+                  readonly
                 />
               </div>
             </div>
@@ -101,11 +102,12 @@
             <textarea
               class="text-justify shadow appearance-none border rounded w-full py-7 px-3 mt-5 leading-tight focus:outline-none focus:shadow-outline input"
               placeholder="HTML, CSS, JavaScript, React, vue.js, GIT, PHP, MySQL. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet quam justo. Aliquam interdum, facilisis eros, ut dapibus quam hendrerit nec."
-              name="requiredSkills"
+              name="requiredSkills"  id="requiredSkills"
               cols="20"
               rows="4"
               v-model="requiredSkills"
             ></textarea>
+            <p v-if="isError.requiredSkills" class="col-start-4 col-span-9 text-red-500 text-md  italic">Veuillez inscrire les compétences requises de l'offre</p>
           </div>
         </div>
   
@@ -119,6 +121,7 @@
                   <option disabled value="">Sélectionnez une province</option>
                   <option v-for="province in provincesListe" :key="province._id" :value="province._id">{{ province.value }}</option>
                 </select>
+                <p v-if="isError.selectedProvince" class="col-start-4 col-span-9 text-red-500 text-md italic">Veuillez séléctionner une province</p>
               </div>
             </div>
   
@@ -129,9 +132,9 @@
                   class="shadow appearance-none border rounded w-full py-7 px-3 mt-5 leading-tight focus:outline-none focus:shadow-outline input"
                   placeholder="Ex.: 35"
                   type="text"
-                  v-model="weeklyWorkHours" name="weeklyWorkHours"
-                  required
+                  v-model="weeklyWorkHours" name="weeklyWorkHours" id="weeklyWorkHours"
                 />
+                <p v-if="isError.weeklyWorkHours" class="col-start-4 col-span-9 text-red-500 text-md  italic">Veuillez inscrire le nombre d'heures par semaine de l'offre</p>
               </div>
             </div>
             <div class="DemandeStage__section__info__item">
@@ -142,9 +145,9 @@
                   placeholder="Ex.: 32"
                   type="text"
                   v-model="salary"
-                  name="salary"
-                  required
+                  name="salary" id="salary"
                 />
+                <p v-if="isError.salary" class="col-start-4 col-span-9 text-red-500 text-md italic">Veuillez inscrire le salaire de l'offre</p>
               </div>
             </div>
           </div>
@@ -158,18 +161,20 @@
                   <option v-for="internshipType in internshipTypesListe" :key="internshipType._id" :value="internshipType._id">{{ internshipType.value }}</option>
                 </select>
               </div>
+              <p v-if="isError.selectedType" class="col-start-4 col-span-9 text-red-500 text-md italic">Veuillez séléctionner un type d'offre</p>
             </div>
   
             <div class="DemandeStage__section__info__item">
               <div class="mb-16 mr-2">
                 <label class="text-bg font-bold" for="paid">Rémunération</label>
 
-                <select  v-model="paid" name="paid"  class="shadow appearance-none border rounded w-full py-7 px-3 mt-5 leading-tight focus:outline-none focus:shadow-outline input">
+                <select  v-model="paid" name="paid" class="shadow appearance-none border rounded w-full py-7 px-3 mt-5 leading-tight focus:outline-none focus:shadow-outline input">
                   <option disabled value="">Sélectionnez une type de rémunération</option>
                   <option value="DISCRETIONARY">DISCRETIONARY</option>
                   <option value="PAID">PAID</option>
                   <option value="UNPAID">UNPAID</option>
                 </select>
+                <p v-if="isError.paid" class="col-start-4 col-span-9 text-red-500 text-md italic">Veuillez séléctionner un type de rémunération</p>
               </div>
             </div>
   
@@ -179,9 +184,9 @@
                 <input
                   class="shadow appearance-none border rounded w-full py-7 px-3 mt-5 leading-tight focus:outline-none focus:shadow-outline input"
                   type="date"
-                  v-model="startDate"  name="startDate"
-                  required
+                  v-model="startDate"  name="startDate" id="startDate"
                 />
+                <p v-if="isError.startDate" class="col-start-4 col-span-9 text-red-500 text-md italic">Veuillez inscrire la date de début de l'offre</p>
               </div>
             </div>
   
@@ -191,9 +196,9 @@
                 <input
                   class="shadow appearance-none border rounded w-full py-7 px-3 mt-5 leading-tight focus:outline-none focus:shadow-outline input"
                   type="date"
-                  v-model="endDate" name="endDate"
-                  required
+                  v-model="endDate" name="endDate" id="endDate"
                 />
+                <p v-if="isError.endDate" class="col-start-4 col-span-9 text-red-500 text-md italic">Veuillez inscrire la date de fin de l'offre</p>
               </div>
             </div>
           </div>
@@ -203,17 +208,22 @@
   
     <div class="DemandeStage__btn">
       <button
+      class="text-slate-400 border-solid text-2xl border-slate-400 border-2 py-4 px-4 mt-24 rounded-lg focus:outline-none focus:shadow-outline"
+      @click="cancel">
+      <div class="">Annuler</div>
+    </button>
+      <button
         class="flex items-center text-white border-solid text-2xl py-4 px-4 mt-24 rounded-lg focus:outline-none focus:shadow-outline btn"
-        @click="save"
+        @click="saveAndValidate"
       >
         Ajouter
       </button>
     </div>
-  </form>
+ 
   </template>
   
   <script setup>
-  import { ref, onMounted, watch } from "vue";
+  import { ref, onMounted, watch, reactive } from "vue";
   import { useRouter } from "vue-router";
   import InternshipOffersService from "../services/internshipOffers/internshipOffersServices";
   import EnterpriseService from "../services/enterprises/enterprisesServices";
@@ -228,8 +238,7 @@
       const description = ref("");
       const selectedEnterprise = ref("");
 
-//entreprise
-// Variables pour stocker les informations de l'entreprise
+      //entreprise
       const id_enterprise  = ref("");
       const image_enterprise = ref("");
       const name_enterprise = ref("");
@@ -270,9 +279,14 @@
       const { addInternshipOffer } = InternshipOffersService();
       
       const save = async () => {
-      const skillsArray = requiredSkills.value.split(",").map(skill => skill.trim());
+
+        let skillsArray = [];
+        if (requiredSkills.value) {
+            skillsArray = requiredSkills.value.split(",").map(skill => skill.trim());
+        }
+
       const newOffer = {
-      
+
     title: title.value,
     description: description.value,
     enterprise: {
@@ -312,18 +326,15 @@
     
     isActive: false,
     };
-    
-  
+
   try {
     await addInternshipOffer(newOffer);
-    console.log("Nouvelle offre de stage ajoutée avec succès !");
-    console.log(newOffer);
     showSuccessMessage.value = true;
-
+    console.log(newOffer);
   } catch (error) {
     console.error("Erreur lors de l'ajout de l'offre de stage :", error);
+    console.log('erreuuuur');
   }
-
 
 };
   
@@ -391,8 +402,67 @@
     loadTypeInfo();
   });
 
-
+  function cancel() {
+    router.go(-1);
+  }
   // validation : 
 
+  const isError = reactive({
+  title: false,
+  description: false,
+  requiredSkills: false,
+  weeklyWorkHours: false,
+  selectedEnterprise: false,
+  paid: false,
+  startDate: false,
+  endDate: false,
+  salary: false,
+  selectedProvince: false,
+  selectedType: false,
+});
+
+  function onValidate(e) {
+  e.preventDefault();
+
+  if ( title.value === "") isError.title  = true;
+  else isError.title  = false;
+
+  if (description.value === "") isError.description = true;
+  else isError.description = false;
+
+  if (selectedEnterprise.value === "") isError.selectedEnterprise = true;
+  else isError.selectedEnterprise = false;
+
+  if (!requiredSkills.value || (Array.isArray(requiredSkills.value) && requiredSkills.value.length === 0)) isError.requiredSkills = true;
+  else isError.requiredSkills = false;
+
+  if (selectedProvince.value === "") isError.selectedProvince = true;
+  else isError.selectedProvince  = false;
+
+  if (weeklyWorkHours.value === "") isError.weeklyWorkHours = true;
+  else isError.weeklyWorkHours  = false;
+
+  if (selectedType.value === "") isError.selectedType = true;
+  else isError.selectedType  = false;
+
+  if (paid.value === "") isError.paid = true;
+  else isError.paid  = false;
+
+  if (startDate.value === "") isError.startDate = true;
+  else isError.startDate  = false;
+
+  if (endDate.value === "") isError.endDate = true;
+  else isError.endDate  = false;
+
+  if (salary.value === "") isError.salary = true;
+  else isError.salary  = false;
+}
+  const saveAndValidate = async (e) => {
+    if (e) {
+        e.preventDefault();
+    }
+    onValidate(e);
+    await save(); 
+  };
   </script>
   
