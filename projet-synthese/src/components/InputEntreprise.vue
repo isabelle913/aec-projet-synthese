ce555d7593d243dec17b0619644d037f971dd1e5
isabelle913 <atelierisabelle@videotron.ca>
2024-04-03 21:18:45 -0400

    Finaliser entreprise

--- projet-synthese/src/components/InputEntreprise.vue
+++ projet-synthese/src/components/InputEntreprise.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="border-l-8 border-l-gray-600 px-4">
-    <div class="text-gray-600 mb-4 text-2xl">{{ label }}</div>
+    <div class="text-gray-800 mb-4 text-2xl font-semibold">{{ label }}</div>
     <div v-if="!isEdit" class="text-gray-400">{{ theModel }}</div>
 
     <div v-else>
       <label for="name" class="hidden">{{ label }}</label>
       <input class="w-full border border-gray-400 rounded py-3 px-3 text-gray-600 leading-tight focus:border-gray-800 hover:border-gray-800" id="name" name="name" type="text" v-model="theModel" />
-      <p v-if="isError" class="text-red-500 text-xs italic">Veuillez remplir ce champ.</p>
+      <p v-if="isError" class="text-red-500 text-base italic">Veuillez remplir ce champ.</p>
     </div>
   </div>
 </template>
